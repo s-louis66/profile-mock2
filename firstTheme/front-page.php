d7dd14530a7225a0c19e7a588df7c3f09ccdce65
s-louis66 <60453994+s-louis66@users.noreply.github.com>
2020-03-23 23:08:44 -0400

    Add files via upload

---
+++ firstTheme/front-page.php
@@ -0,0 +1,9 @@
+<?php /* Template Name: front-page */ ?>
+
+<?php get_header(); ?>
+<?php
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+    get_template_part( 'content', get_post_format() );
+endwhile; endif;
+?>
+<?php get_footer(); ?>
